faaa9fb2c73f4bd0b810409a39babe8c325ccbd2
Guzhov Roman <guzovvrom277@gmail.com>
2025-10-06 14:45:33 +0500

    guess_the_number version 01 (Добавлено ограничение попыток)

--- guess_the_number.py
+++ guess_the_number.py
@@ -3,11 +3,7 @@ import random
 min_value, max_value = map(int, input('Введите диапазон чисел через пробел (например: 1 50): ').split())
 hidden_number = random.randint(min_value, max_value)
 
-attempt = 0
-while True:
-
-    if attempt == 3:
-        print('Загаданное число четное') if hidden_number % 2 == 0 else print('Загаданное число нечетное')
+for attempt in range(3):
     number = int(input('Введите число: '))
     if number == hidden_number:
         print('Вы угадали число')
@@ -16,7 +12,8 @@ while True:
         print('Загаданное число меньше')
     else:
         print('Загаданное число больше')
-    attempt += 1
+    if attempt == 2:
+        print('Загаданное число четное') if hidden_number % 2 == 0 else print('Загаданное число нечетное')
     print('<------------------------------------->')
 
 
